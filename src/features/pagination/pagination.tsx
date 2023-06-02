7f4f8b37388a6cb65e4a815c34feb039c62882db
Dmitry Zaytsev <zadima@gmail.com>
2023-06-02 16:57:28 +0400

    create pagination feature

---
+++ src/features/pagination/pagination.tsx
@@ -0,0 +1,9 @@
+export type Pagination = {
+  perPage: number
+  page: number
+}
+
+export const DEFAULT_PAGE_SIZE = 10
+
+export const PAGE_PARAM_KEY = "page"
+export const PER_PAGE_PARAM_KEY = "perPage"
---
+++ src/features/pagination/paginationController.tsx
@@ -0,0 +1,25 @@
+import { changeSearchParams, pushParamsToUrl } from "../../utils/urlQuery"
+import { PAGE_PARAM_KEY, PER_PAGE_PARAM_KEY, Pagination } from "./pagination"
+import { DEFAULT_PAGE_SIZE } from "./pagination"
+
+export function getPaginationParams(): Pagination {
+  const params = new URLSearchParams(window.location.search)
+  return {
+    perPage: Number(params.get(PER_PAGE_PARAM_KEY) ?? DEFAULT_PAGE_SIZE),
+    page: Number(params.get(PAGE_PARAM_KEY) ?? 1),
+  }
+}
+
+export function goToPage(page: number): void {
+  pushParamsToUrl(changeSearchParams(PAGE_PARAM_KEY, String(page)))
+}
+
+export function goToNextPage(): void {
+  const currentPage = getPaginationParams().page
+  goToPage(currentPage + 1)
+}
+
+export function goToPrevPage(): void {
+  const currentPage = getPaginationParams().page
+  goToPage(currentPage - 1)
+}
