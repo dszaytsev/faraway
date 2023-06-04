42302d9a2ea7abe77fd734183535e84b5ce607ac
Dmitry Zaytsev <zadima@gmail.com>
2023-06-04 15:10:27 +0400

    install mui lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,38 @@
-import { Route, Switch } from "wouter"
+import { Route, Link as RouterLink, Switch } from "wouter"
 
 import { CharacterDetails } from "./pages/CharacterDetails"
 import { CharacterList } from "./pages/CharacterList"
+import { AppBar, Container, CssBaseline, Link, Toolbar } from "@mui/material"
 
 function App() {
   return (
-    <>
-      <Switch>
-        <Route path="/">
-          <CharacterList />
-        </Route>
+    <div className="App">
+      <CssBaseline />
 
-        <Route path="/character/:id">
-          {(params) => <CharacterDetails characterId={params.id} />}
-        </Route>
+      <AppBar position="sticky" color="primary" sx={{ mb: 4 }}>
+        <Toolbar>
+          <RouterLink href="/">
+            <Link color="white" variant="h5" underline="none">
+              StarWars Characters
+            </Link>
+          </RouterLink>
+        </Toolbar>
+      </AppBar>
 
-        <Route>404</Route>
-      </Switch>
-    </>
+      <Container component="main" maxWidth="md">
+        <Switch>
+          <Route path="/">
+            <CharacterList />
+          </Route>
+
+          <Route path="/character/:id">
+            {(params) => <CharacterDetails characterId={params.id} />}
+          </Route>
+
+          <Route>404</Route>
+        </Switch>
+      </Container>
+    </div>
   )
 }
 
